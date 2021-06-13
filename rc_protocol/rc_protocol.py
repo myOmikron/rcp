1b8183a39c7545432588b81847ad187d6df0d450
myOmikron <accounts@omikron.dev>
2021-06-13 21:00:57 +0200

    RCPs time component is now optional. Enabled by default.

--- rc_protocol/rc_protocol.py
+++ rc_protocol/rc_protocol.py
@@ -7,6 +7,7 @@ def validate_checksum(
         shared_secret: str,
         salt: str = "",
         time_delta: int = 5,
+        use_time_component: bool = True,
         checksum_key: str = "checksum"
 ) -> bool:
     """
@@ -16,12 +17,14 @@ def validate_checksum(
     :param shared_secret: Shared secret the request is hashed with
     :param salt: Additional salt to use. Defaults to empty str
     :param time_delta: Time delta (+ and -) the requests should be valid, in seconds
+    :param use_time_component: If specified False, time is not used for the protocol
     :param checksum_key: The key to access to checksum in request. Defaults to "checksum".
 
     :return: bool
     """
-    # Get current (utc) timestamp
-    current_timestamp = int(datetime.now(timezone.utc).timestamp())
+    if use_time_component:
+        # Get current (utc) timestamp
+        current_timestamp = int(datetime.now(timezone.utc).timestamp())
 
     # Save checksum and delete from dict
     if checksum_key not in request:
@@ -38,15 +41,21 @@ def validate_checksum(
     # Append with shared_secret
     static_str += shared_secret
 
-    # Iterate over the last +- time_delta timestamps
-    for i in range(-time_delta, time_delta):
-        tmp_timestamp = current_timestamp + i
+    if use_time_component:
+        # Iterate over the last +- time_delta timestamps
+        for i in range(-time_delta, time_delta):
+            tmp_timestamp = current_timestamp + i
 
-        # Append static_str with timestamp
-        not_so_static_str = static_str + str(tmp_timestamp)
+            # Append static_str with timestamp
+            not_so_static_str = static_str + str(tmp_timestamp)
 
+            # Add salt and hash with SHA512
+            hashed = hashlib.sha512((salt + not_so_static_str).encode("utf-8")).hexdigest()
+            if hashed == checksum:
+                return True
+    else:
         # Add salt and hash with SHA512
-        hashed = hashlib.sha512((salt + not_so_static_str).encode("utf-8")).hexdigest()
+        hashed = hashlib.sha512((salt + static_str).encode("utf-8")).hexdigest()
         if hashed == checksum:
             return True
     return False
@@ -55,6 +64,7 @@ def validate_checksum(
 def get_checksum(
         request: dict,
         shared_secret: str,
+        use_time_component: bool = True,
         salt: str = ""
 ) -> str:
     """
@@ -63,11 +73,13 @@ def get_checksum(
     :param request: The dictionary with all parameters
     :param shared_secret: Shared secret the request is hashed with
     :param salt: Additional salt to use. Defaults to empty str
+    :param use_time_component: If specified False, time is not used for the protocol
 
     :return: Checksum str
     """
-    # Get current (utc) timestamp
-    current_timestamp = int(datetime.now(timezone.utc).timestamp())
+    if use_time_component:
+        # Get current (utc) timestamp
+        current_timestamp = int(datetime.now(timezone.utc).timestamp())
 
     # Build sorted list
     sorted_request = [key + str(request[key]) for key in sorted(request)]
@@ -78,8 +90,9 @@ def get_checksum(
     # Append with shared_secret
     static_str += shared_secret
 
-    # Append with timestamp
-    static_str += str(current_timestamp)
+    if use_time_component:
+        # Append with timestamp
+        static_str += str(current_timestamp)
 
     # Add salt and hash with SHA512
     hashed = hashlib.sha512((salt + static_str).encode("utf-8")).hexdigest()
